525b46b527634b7b73c679ad126f6167d0da44ea
clindsey4 <clindsey4@murraystate.edu>
2024-02-25 19:56:51 -0600

    Add basic components.

---
+++ src/components/material/filled-button.tsx
@@ -0,0 +1,46 @@
+import { MouseEventHandler } from "react";
+import { Icon } from "./icon";
+import Link from "next/link";
+
+export function FilledButton(
+    {
+        text,
+        icon,
+        href,
+        onClick,
+        className = '',
+        disabled
+    }: {
+        text: string,
+        icon?: string,
+        href?: string,
+        onClick?: MouseEventHandler,
+        className?: string,
+        disabled?: boolean
+    }
+) {
+    const iconElement = icon ? <Icon icon={icon}/> : undefined
+    
+    return (
+        href ? (
+            <Link
+                aria-disabled={disabled}
+                className={`text-base bg-primary text-on-primary h-10 rounded-full flex items-center justify-center relative before:transition-opacity before:absolute before:w-full before:h-full before:left-0 before:top-0 before:rounded-full before:opacity-0 before:bg-on-primary before:hover:opacity-[0.12] transition-opacity ${icon ? 'pl-4 pr-6 gap-2' : 'px-6'} ${className}`}
+                href={href}
+                onClick={onClick}
+            >
+                {iconElement}
+                {text}
+            </Link>
+        ): (
+            <button
+                aria-disabled={disabled}
+                className={`text-base bg-primary text-on-primary h-10 rounded-full flex items-center justify-center relative before:transition-opacity before:absolute before:w-full before:h-full before:left-0 before:top-0 before:rounded-full before:opacity-0 before:bg-on-primary before:hover:opacity-[0.12] transition-opacity ${icon ? 'pl-4 pr-6 gap-2' : 'px-6'} ${className}`}
+                onClick={onClick}
+            >
+                {iconElement}
+                {text}
+            </button>
+        )
+    )
+}
