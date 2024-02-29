1d61032218d5bdb7938a48c3dc4b24488a7fad6d
awatkins23 <awatkins23@murraystate.edu>
2024-02-28 23:18:55 -0600

    Made it where < large screens have 1 column

--- src/app/[lang]/news/page.tsx
+++ src/app/[lang]/news/page.tsx
@@ -17,11 +17,11 @@ export default async function News() {
     return (
         <article className="w-full max-w-6xl flex flex-col gap-5 mt-20  text-on-surface">
             <section className="w-full flex flex-row justify-between align-middle">
-                <h1 className="text-on-surface font-bold text-4xl ">Newsfeed</h1>
+                <h1 className="text-on-surface font-bold text-4xl ">News</h1>
                 <FilledButton text="New Post" className=""/>
             </section>
             <Divider />
-            <section className="w-full max-w-6xl grid grid-cols-1  md:grid-cols-3 gap-5 text-on-surface">
+            <section className="w-full max-w-6xl grid grid-cols-1  lg:grid-cols-3 gap-5 text-on-surface">
                 {
                     data.map(announcement => {
                         return <NewsCard news={announcement}></NewsCard>
