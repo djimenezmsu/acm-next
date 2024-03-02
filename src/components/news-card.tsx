bf3bb138491ef7593f56b5740f6a4e01c1bda352
awatkins23 <awatkins23@murraystate.edu>
2024-03-02 14:23:56 -0600

    removed break-all from news-card

--- src/components/news-card.tsx
+++ src/components/news-card.tsx
@@ -6,7 +6,7 @@ export async function NewsCard(
     {news, buttonText}: {news:News, buttonText: string},
 ){
     return (
-        <li className="lg:w-100% bg-surface-container p-4 rounded-2xl flex flex-col gap-5 h-fit text-wrap break-all">
+        <li className="lg:w-100% bg-surface-container p-4 rounded-2xl flex flex-col gap-5 h-fit text-wrap">
             <p className="text-2xl">{news.title}</p>
             <p className="text-s">{news.postDate.toLocaleDateString()}</p>
             <p>{news.subject}</p>
